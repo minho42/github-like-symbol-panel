a23aaca469785937c96e4fd30feb980e374c691f
Min ho Kim <minho42@gmail.com>
2023-08-18 13:07:18 +1000

    remove metadata

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,5 @@
 import "./globals.css";
 
-export const metadata = {
-  title: "Symbols panel",
-  description: "GitHub-like Symbols panel",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
